657a198732884e0c58f4a867fb7429e82ad76fa5
Alfonso Esposito <alfonso.esposito6@unibo.it>
2026-04-09 21:09:32 +0200

    update fix

--- config/database.php
+++ config/database.php
@@ -87,10 +87,10 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', 'db.nutrimente.supabase.co'),
+            'host' => env('DB_HOST', 'db.quuhzpnjfqgygeozuqbp.supabase.co'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
+            'database' => env('DB_DATABASE', 'postgres'),
+            'username' => env('DB_USERNAME', 'postgres.quuhzpnjfqgygeozuqbp'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
